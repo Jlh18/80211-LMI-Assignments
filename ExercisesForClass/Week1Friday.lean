1430a036ee8f79cea337b96de259c26b316abb00
jlh18 <josephhua73@yahoo.com>
2024-06-27 12:16:31 -0400

    translation+nat_ded

---
+++ ExercisesForClass/Week1Friday.lean
@@ -0,0 +1,44 @@
+/- 
+P := I am pescatarian
+V := I am vegetarian
+N := I am vegan
+F := I eat fish
+
+P1 := the food is pescatarian
+V1 := the food is vegetarian
+N1 := the food is vegan
+
+E := I can eat the food
+-/
+
+/- 
+Translate the following to formulas in propositional logic 
+
+If I am vegan then I am vegetarian.
+
+I am vegetarian if and only if I am pescatarian and I don't eat fish.
+
+If I am vegan then I can eat the vegan food.
+
+If I am vegetarian then I can eat the vegetarian food.
+
+If the food is vegan then it is vegetarian.
+
+Translate the following to formulas. Then from the above, prove: 
+
+If I am vegetarian then I am pescatarian.
+
+If I am vegan then I am pescaterian.
+
+I am pescatarian and vegetarian if and only if I am vegetarian.
+
+If I am vegan then I don't eat fish.
+
+If I eat fish then I am not vegan.
+
+If I can eat vegetarian food then I can eat vegan food.
+
+If I cannot eat the vegetarian food then I am not pescatarian.
+-/
+
+
